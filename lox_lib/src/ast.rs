61179f6ef85f8a1347715b5965ef33dd7bdff4e7
Diego Marcano <marcanp.diego@gmail.com>
2021-11-30 21:40:32 -0800

    second ast impl

--- lox_lib/src/ast.rs
+++ lox_lib/src/ast.rs
@@ -3,47 +3,37 @@
 //!
 use crate::lexer::Token;
 
+#[derive(Debug, Clone)]
+pub enum Operator { 
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    GreaterThan,
+    LessThan,
+    Equal,
+    NotEqual,
+    And,
+    Or,
+    Bang, 
+}
+
 
 #[derive(Debug, Clone)]
-pub enum Expression {
-    Primary(Primary),
-}
-
-pub enum Term { 
-    Factor(Factor),
-    Add(Box<Term>, Box<Term>),
-    Sub(Box<Term>, Box<Term>),
-}
-
-
-#[derive(Debug)]
-/// A factor expression is an expression that uses the `*` or `/` operators. It follows the grammar 
-/// 
-///  factor => unary (("*" | "/") unary)*
-pub enum Factor {
-    Unary(Unary),
-    Times(Box<Factor>, Box<Factor>),
-    Divide(Box<Factor>, Box<Factor>),
-}
-
-#[derive(Debug, Clone)]
-/// A primary expression contains the literals and the grouping expressions
-pub enum Primary {
-    Number(f32),
+pub enum Node { 
     String(String),
-    True,
-    False,
-    Nil,
-    Expression(Box<Expression>),
-}
-
-#[derive(Debug)]
-/// A unary expression is an expression which follows the grammar
-///
-/// unary => ("!" | "-") unary | primary
-///
-pub enum Unary {
-    Bang(Box<Unary>),
-    Minus(Box<Unary>),
-    Primary(Primary),
+    Number(f32),
+    True, 
+    False, 
+    Nil, 
+    Grouping(Box<Node>),
+    UnaryExpr{ 
+        operator: Operator, 
+        right: Box<Node> 
+    },
+    BinaryExpr{ 
+        left: Box<Node>, 
+        operator: Operator, 
+        right: Box<Node> 
+    },
 }

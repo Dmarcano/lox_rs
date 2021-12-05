a72bc6460055710a5d431a98a4c573cb486e568d
Diego Marcano <marcanp.diego@gmail.com>
2021-12-05 01:14:17 -0800

    parsing helper function

--- lox_lib/src/parser.rs
+++ lox_lib/src/parser.rs
@@ -4,7 +4,7 @@ use crate::lexer::{Token, TokenType};
 /// a parser for the Lox language. It creates an Abstract Syntax Tree (AST) from a token stream.
 pub struct Parser {}
 
-type ParserBinaryFn = fn(&Parser ) -> Node; 
+type ParserBinaryFn = fn(&Parser) -> Node;
 
 /*
  Reference Lox Grammar (So far)
@@ -25,32 +25,19 @@ type ParserBinaryFn = fn(&Parser ) -> Node;
     primary         -> NUMBER | STRING | "True" | "False" | "Nil" | "("expression")" ;
 */
 impl Parser {
-
     pub fn new() -> Self {
         Self {}
     }
 
-    pub fn expression(&self) -> Node {
-        self.equality()
-    }
-
-    // equality  -> comparison ( ("!=" | "==") comparison )* ;
-    pub fn equality(&self) -> Node {
-        let mut node = self.comparison();
-
-        while let Some(operator) = self.match_tokens(&[TokenType::BangEqual, TokenType::EqualEqual]) {
-            let right = self.expression();
-            node = Node::BinaryExpr {
-                operator: operator,
-                left: Box::new(node),
-                right: Box::new(right),
-            };
-        }
-        node
-    }
-
-    fn binary_expression_match(&self, paring_fn : ParserBinaryFn, token_types : &[TokenType]) -> Node { 
-        let mut node = paring_fn(self);
+    /// This function is used to simplify the implementation of binary expressions. By taking  
+    /// advantage of the fact that the grammar for most binary expressions is very similiar 
+    /// 
+    fn binary_expression_match(
+        &self,
+        precedence_fn: ParserBinaryFn,
+        token_types: &[TokenType],
+    ) -> Node {
+        let mut node = precedence_fn(self);
 
         while let Some(operator) = self.match_tokens(token_types) {
             let right = self.expression();
@@ -62,6 +49,24 @@ impl Parser {
         }
         node
     }
+
+
+    pub fn expression(&self) -> Node {
+        self.equality()
+    }
+
+    /// Performs a binary equality operation on possible expressions. It follows the following grammar.
+    /// 
+    /// 
+    /// `equality  -> comparison ( ("!=" | "==") comparison )* ;`
+    pub fn equality(&self) -> Node {
+
+        self.binary_expression_match(
+            Parser::comparison,
+            &[TokenType::BangEqual, TokenType::EqualEqual],
+        )
+    }
+
 
     pub fn comparison(&self) -> Node {
         todo!()
@@ -94,19 +99,18 @@ impl Parser {
     }
 }
 
-
 #[cfg(test)]
-mod test { 
+mod test {
     use super::*;
 
     #[test]
-    fn expression_test() { 
+    fn expression_test() {
         todo!()
     }
 
     #[test]
-    fn equality_test() { 
-        // testing the equality of the following expression 
+    fn equality_test() {
+        // testing the equality of the following expression
         // a == b == c != d
         let tokens = [
             Token::new(TokenType::Identifier, "a".to_string(), 0),
